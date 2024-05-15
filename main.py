e027b756b159dbbd8e0182302565936c6581a16f
brancovic <huskabranko@gmail.com>
2024-05-15 21:13:32 +0200

    functioning + fixed bug with loging in using sapces or empty argument + added nicer GUI using Tkinter + presentable +fixed bug with registering existing users again

--- main.py
+++ main.py
@@ -1,50 +1,60 @@
 import json
 
-# Login part
-logged = True
+# Login function
+def login():
+    logged = True
 
-while logged:
+    while logged:
+        action = input("Enter action you want to make or quit (Login, register, quit): ")
+        action = action.lower()
 
-    action = input("Enter action you want to make or quit (Login, register, quit): ")
-    action = action.lower()
+        if action == "login":
+            username = input("Enter username: ")
+            password = input("Enter {} password: ".format(username))
 
-    if action == "login":
-        username = input("Enter username: ")
-        password = input("Enter {} password: ".format(username))
+            if username == "quit" or password == quit:
+                print("returning to menu")
+                continue
 
-        if username == "quit" or password == quit:
-            print("returning to menu")
+            with open('usernames.json', 'r') as usernames_file:
+                username_data = json.load(usernames_file)
 
-        with open('usernames.json', 'r') as usernames_file:
-            username_data = json.load(usernames_file)
+            with open('passwords.json', 'r') as passwords_file:
+                password_data = json.load(passwords_file)
 
-        with open('passwords.json', 'r') as passwords_file:
-            password_data = json.load(passwords_file)
-
-        if username in username_data['id']:
-            index = username_data['id'].index(username)
-            if password == password_data['password'][index]:
-                print("Welcome, {}!".format(username))
-                logged = False
+            if username in username_data['id']:
+                index = username_data['id'].index(username)
+                if password == password_data['password'][index]:
+                    print("Welcome, {}!".format(username))
+                    logged = False
+                else:
+                    print("Incorrect password.")
             else:
-                print("Incorrect password.")
-        else:
-            print("Username not found.")
+                print("Username not found.")
 
-    elif action == "register":
-        username = input("Enter new username: ")
-        password = input("Enter new password: ")
-        if username == "quit" or password == "quit":
-            print("returning to menu")
+        elif action == "register":
+            username = input("Enter new username: ")
+            password = input("Enter new password: ")
 
-        if "" == username or " " in username:
-            print("Invalid arguments (cannot use spaces)")
-        else:
+            if username == "quit" or password == "quit":
+                print("returning to menu")
+                continue
+
+            if "" == username or " " in username:
+                print("Invalid arguments (cannot use spaces)")
+                continue
+
             with open('usernames.json', 'r+') as usernames_file:
                 usernames_data = json.load(usernames_file)
-                usernames_data['id'].append(username)
-                usernames_file.seek(0)
-                json.dump(usernames_data, usernames_file)
+
+                if username in usernames_data['id']:
+                    print("Username already exists")
+                    continue
+
+                else:
+                    usernames_data['id'].append(username)
+                    usernames_file.seek(0)
+                    json.dump(usernames_data, usernames_file)
 
             with open('passwords.json', 'r+') as passwords_file:
                 passwords_data = json.load(passwords_file)
@@ -52,8 +62,13 @@ while logged:
                 passwords_file.seek(0)
                 json.dump(passwords_data, passwords_file)
                 print("User {} registered successfully.".format(username))
-    elif action == "quit":
-        print("Quitting program")
-        break
-    else:
-        print("False input, try again")
+
+        elif action == "quit":
+            print("Quitting program")
+            break
+
+        else:
+            print("False input, try again")
+
+# Call the login function
+login()
